ca7da584ded92cd4ee63b12fc2222e6b81c4869a
Valiner Osgordov <46810709+valinerosgordov@users.noreply.github.com>
2026-04-03 23:49:32 +0300

    fix: add migration for Order guest fields

--- src/Nuts.Infrastructure/Migrations/20260403204922_AddOrderGuestFields.Designer.cs
+++ src/Nuts.Infrastructure/Migrations/20260403204922_AddOrderGuestFields.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Nuts.Infrastructure.Persistence;
 
@@ -10,9 +11,11 @@ using Nuts.Infrastructure.Persistence;
 namespace Nuts.Infrastructure.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260403204922_AddOrderGuestFields")]
+    partial class AddOrderGuestFields
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "10.0.5");
